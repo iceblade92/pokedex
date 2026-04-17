a557d07f85fcf305cb7664ce31b7f4227b7c1e1a
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-04-17 14:02:09 +0200

    REPL test for main loop

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -10,4 +13,20 @@ func cleanInput(text string) []string {
 	str = strings.TrimSpace(str)
 	result := strings.Fields(str)
 	return result
+}
+
+func Repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		str := cleanInput(text)
+		if len(str) == 0 {
+			fmt.Println("No text prompt provided")
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", str[0])
+
+	}
 }
