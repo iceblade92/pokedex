0eea289a93faa3c0e42aa839f5569f0d886c8245
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-04-21 14:44:33 +0200

    user comands like exit and help

--- repl.go
+++ repl.go
@@ -7,6 +7,28 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	command := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Help usage for Pokedex",
+			callback:    commandHelp,
+		},
+	}
+	return command
+}
+
 func cleanInput(text string) []string {
 	str := text
 	str = strings.ToLower(str)
@@ -26,7 +48,25 @@ func Repl() {
 			fmt.Println("No text prompt provided")
 			continue
 		}
-		fmt.Printf("Your command was: %s\n", str[0])
-
+		command, exists := getCommands()[str[0]]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
 }
